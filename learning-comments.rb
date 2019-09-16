6faad8f4b752a2c7c47ed5f9a9c34ce3126e4fd0
Megan Adams <meganoliviawendyadams@gmail.com>
2019-09-16 14:01:56 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,23 @@
+games = ["Pokemon", "Skyrim"]
+puts ' '
+puts ' '
+puts 'So tell me, !###ERROR---40-HASHTAGPERSO-Lol nah just kidding. Tell me m8, What is your fav game?'
+gamegame = gets.strip
+games.push(gamegame)
+if games[2] == games[0]
+games[0] = 'the original Red/Blue versions'
+end
+puts ' '
+puts "Oh, THE #{gamegame}!?"
+puts ' '
+
+
+puts "I like #{games[1]} the most, #{games[2]} and #{games[0]} are deffo up there in my favs."
+while games[0] == 'Pokemon'
+if games[0] = 'the original Red/Blue versions'
+break
+
+
+puts ' '
+end
+end
